846a4969a34bd647b4e340818494d16f8cf75f79
mlamp <margus.lamp@gmail.com>
2012-10-03 03:03:04 +0300

    removed console.log and added some detection when there is no scores
    Change-Id: I2e65ff14637c5776ae72d644510e8c03256923ff

--- src/contentScript.js
+++ src/contentScript.js
@@ -4,9 +4,7 @@ var port = chrome.extension.connect({name: "content-background"});
 port.onMessage.addListener(function(msg) {
 	if (msg.response == 'movies') {
 		var rottentomato_response = msg.response_data;
-		console.log(rottentomato_response);
 		if (rottentomato_response.total > 0) {
-			
 			/* Let's try to figure out if we have better result when we're looping and trying to figure out right movie id */
 			var rottentomato_movie = null;
 			$(rottentomato_response.movies).each(function(index, movie_item) {
@@ -19,7 +17,30 @@ port.onMessage.addListener(function(msg) {
 			if (!rottentomato_movie) {
 				rottentomato_movie = rottentomato_response.movies[0];
 			}
-			var movie_avg_score = (rottentomato_movie.ratings.critics_score + rottentomato_movie.ratings.audience_score) / 2;
+			
+			var movie_total_score = 0;
+			var parties_rated = 0;
+			var is_rated = false;
+			if (rottentomato_movie.ratings.critics_score > 0) {
+				movie_total_score += rottentomato_movie.ratings.critics_score;
+				++parties_rated;
+				is_rated = true;
+			}
+			
+			if (rottentomato_movie.ratings.audience_score > 0) {
+				movie_total_score += rottentomato_movie.ratings.audience_score;
+				++parties_rated;
+				is_rated = true;
+			}
+			
+			var movie_avg_score;
+			if (is_rated) {
+				movie_avg_score = movie_total_score / parties_rated;
+			}
+			else {
+				movie_avg_score = 0;
+			}
+			
 			var is_rotten = true;
 			if (movie_avg_score < 50) {
 				is_rotten = true;
@@ -27,16 +48,12 @@ port.onMessage.addListener(function(msg) {
 			else {
 				is_rotten = false;
 			}
-			//console.log(chrome.extension.getURL("icons/rt_lg.png"));
-			var added_html = '<div class="rottenscore"><span itemprop="ratingValue" class="meter' + (is_rotten ? ' rotten' : ' fresh') + ' numeric ">' + rottentomato_movie.ratings.critics_score + '/' + rottentomato_movie.ratings.audience_score + '</span></div><br />';
-				//<img src="' + chrome.extension.getURL("icons/rt_lg.png") + '" /></td>'
-			//+ '<td>' + rottentomato_movie.ratings.critics_score + '/' + rottentomato_movie.ratings.audience_score + '</td></tr></table>';
-			added_html = '' + 
+			var added_html = '' + 
 			'<div class="general">' +
 				'<div class="info stars">' +
 					'<h5>Rotten Rating:</h5>' +
 					'<div id="rotten_box_outer"><div id="rotten_box" class="' + (is_rotten ? 'rotten' : '') + '"><div class="background"><div class="foreground" style="width: ' + movie_avg_score + '%;"></div></div></div></div>' +
-					'<div class="starbar-meta"><b>' + rottentomato_movie.ratings.critics_score + '/' + rottentomato_movie.ratings.audience_score + '</b>&nbsp;&nbsp;<a href="' + rottentomato_movie.links.alternate + '" class="tn15more" target="_blank">To rotten</a>&nbsp;»</div>' +
+					'<div class="starbar-meta"><b>' + (rottentomato_movie.ratings.critics_score >= 0 ? rottentomato_movie.ratings.critics_score : 'NA') + '/' + ( rottentomato_movie.ratings.audience_score >= 0 ? rottentomato_movie.ratings.audience_score : 'NA') + '</b>&nbsp;&nbsp;<a href="' + rottentomato_movie.links.alternate + '" class="tn15more" target="_blank">To rotten</a>&nbsp;»</div>' +
 				'</div>' +
 			'</div>';
 			$('#tn15rating').append(added_html);
@@ -74,10 +91,3 @@ var query_args = {
 if (full_movie_name) {
 	port.postMessage({action: 'movies', query_args: query_args});
 }
-
-
-
-//
-//$.get("http://api.rottentomatoes.com/api/public/v1.0/movies.json", query_args, function(jsonReturn) {
-//	console.log(jsonReturn);
-//});
